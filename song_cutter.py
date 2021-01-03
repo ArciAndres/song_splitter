d0224aea8c51c6a64cb6d606a02ad9bb207a4c6b
ArciAndres <arciandres@gmail.com>
2021-01-03 01:04:25 +0100

    Successful audio read

--- song_cutter.py
+++ song_cutter.py
@@ -84,33 +84,3 @@ newAudio = audio[0:3000]
 newAudio.export('newSong.mp3', format="mp3") #Exports to a wav file in the current path.
 
 #%%
-
-import os
-import numpy as np
-from matplotlib import pyplot as plt
-import IPython.display as ipd
-import librosa
-import pandas as pd
-
-def print_plot_play(x, Fs, text=''):
-    """1. Prints information about an audio singal, 2. plots the waveform, and 3. Creates player
-    
-    Notebook: C1/B_PythonAudio.ipynb
-    
-    Args: 
-        x: Input signal
-        Fs: Sampling rate of x    
-        text: Text to print
-    """
-    print('%s Fs = %d, x.shape = %s, x.dtype = %s' % (text, Fs, x.shape, x.dtype))
-    plt.figure(figsize=(8, 2))
-    plt.plot(x, color='gray')
-    plt.xlim([0, x.shape[0]])
-    plt.xlabel('Time (samples)')
-    plt.ylabel('Amplitude')
-    plt.tight_layout()
-    plt.show()
-    ipd.display(ipd.Audio(data=x, rate=Fs))
-# Read mp3
-x, Fs = librosa.load(str(song_path))
-#x, Fs = librosa.load(librosa.util.example_audio_file())
